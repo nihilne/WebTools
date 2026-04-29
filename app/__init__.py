654899c7b1e9fe7afa7a5f01f740752048b8c41b
nihilne <85189523+nihilne@users.noreply.github.com>
2026-04-30 00:49:23 +0300

    refactor: handle all 4xx exceptions with one handler

--- app/__init__.py
+++ app/__init__.py
@@ -10,18 +10,19 @@ def create_app():
     for bp in all_blueprints:
         app.register_blueprint(bp)
 
-    @app.errorhandler(BadRequest)
-    def handle_bad_requests(error):
-        return render_template("errors/400.html", error=error), 400
-
     @app.errorhandler(NotFound)
     def handle_not_found(error):
         if request.headers.get("HX-Request"):
-            return (
-                "<div class='text-red-500'>404 Not Found: This does not exist!?</div>",
-                404,
-            )
-        return render_template("errors/404.html"), 404
+            return "<div class='text-red-500'>404 Not Found</div>", 404
+
+        return render_template("errors/404.html", error=error), 404
+
+    @app.errorhandler(HTTPException)
+    def handle_http_exception(error):
+        if 400 <= error.code < 500:
+            return render_template("errors/4xx.html", error=error), error.code
+
+        return error
 
     @app.context_processor
     def inject_context():
