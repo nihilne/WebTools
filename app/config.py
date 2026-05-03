61beb62959a1c8f514f105326a9a182f6e99d8b4
nihilne <85189523+nihilne@users.noreply.github.com>
2026-05-03 20:48:10 +0300

    refactor: build database uri in config

--- app/config.py
+++ app/config.py
@@ -8,12 +8,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     APP_VERSION = "v1.3.1"
 
-    NAV_ITEMS = [
-        {"name": "Random Generator", "endpoint": "randomgen.randomgen"},
-        {"name": "CSV File Splitter", "endpoint": "csvsplitter.csvsplitter"},
-        {"name": "VAT Calculator", "endpoint": "vatcalc.vatcalc"},
-    ]
-
     SESSION_TYPE = "cachelib"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
@@ -23,5 +17,13 @@ class Config:
         default_timeout=3600,
     )
 
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://{user}:{password}@{host}:{port}/{db}".format(
+            user="webtools_user",
+            password=os.environ["DB_USER_PASSWORD"],
+            host="db",
+            port="3306",
+            db="webtools",
+        )
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
