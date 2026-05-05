654e687801f06dd28a118fba4cbf54c2f99cbed9
nihilne <85189523+nihilne@users.noreply.github.com>
2026-05-06 00:55:20 +0300

    feat: add ability to load blueprints on routes subdirectories

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -5,8 +5,8 @@ from flask import Flask
 
 def register_blueprints(app: Flask):
     prefix = __name__ + "."
-    for _, module_name, _ in pkgutil.iter_modules(__path__):
-        module = importlib.import_module(prefix + module_name)
+    for _, module_name, _ in pkgutil.walk_packages(__path__, prefix):
+        module = importlib.import_module(module_name)
         if hasattr(module, "bp"):
             app.register_blueprint(module.bp)
             app.logger.debug(f"Registered blueprint: {module.bp.name}")
