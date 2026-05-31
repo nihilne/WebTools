00f1dee0c1830d993cb1ee3981e22e9b79a610c5
nihilne <85189523+nihilne@users.noreply.github.com>
2026-05-31 17:02:28 +0300

    feat: minecraft offline uuid generator

--- app/services/randomgen_service.py
+++ app/services/randomgen_service.py
@@ -13,3 +13,8 @@ class RandomGenService:
     @staticmethod
     def generate_uuidv4():
         return uuid.uuid4()
+
+    @staticmethod
+    def generate_minecraft_uuid(username: str):
+        """Generates the offline UUID of a Minecraft username"""
+        return uuid.uuid3(uuid.NAMESPACE_OID, f"OfflinePlayer:{username.strip}")
